927e7b99e1c1755d087fb0ec6016df2434bfb24d
Jis van Overschot <99040091@mydavinci.nl>
2018-06-29 09:12:52 +0200

    Api config with participants table data

--- intro_tour/routes/api.php
+++ intro_tour/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Participant;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,31 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('participants', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Participant::all();
+});
+ 
+Route::get('participants/{id}', function($id) {
+    return Participant::find($id);
+});
+
+Route::post('participants', function(Request $request) {
+    return Participant::create($request->all);
+});
+
+Route::put('participants/{id}', function(Request $request, $id) {
+    $participant = Participant::findOrFail($id);
+    $participant->update($request->all());
+
+    return $participant;
+});
+
+Route::delete('participants/{id}', function($id) {
+    Participant::find($id)->delete();
+
+    return 204;
 });
