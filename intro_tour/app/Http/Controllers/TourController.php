ffcc6c069713b0e0ec5931f798a84ddce950c4c6
Jis van Overschot <99040091@mydavinci.nl>
2018-06-29 11:17:28 +0200

    Added admin and tours to api

---
+++ intro_tour/app/Http/Controllers/TourController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tour;
+use Illuminate\Http\Request;
+
+class TourController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Tour::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tour = Tour::create($request->all());
+
+        return response()->json($tour, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tour  $tour
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tour $tour)
+    {
+        return $tour;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tour  $tour
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tour $tour)
+    {
+        $tour->update($request->all());
+
+        return response()->json($tour, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tour  $tour
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(Tour $tour)
+    {
+        $tour->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- intro_tour/routes/api.php
+++ intro_tour/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use App\Participant;
+use App\Admin;
+use App\Tour;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/* Participants routes */
 Route::get('participants', function() {
     // If the Content-Type and Accept headers are set to 'application/json', 
     // this will return a JSON structure. This will be cleaned up later.
@@ -41,6 +44,62 @@ Route::put('participants/{id}', function(Request $request, $id) {
 
 Route::delete('participants/{id}', function($id) {
     Participant::find($id)->delete();
+
+    return 204;
+});
+
+/* Admins */
+Route::get('admins', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Admin::all();
+});
+ 
+Route::get('admins/{id}', function($id) {
+    return Admin::find($id);
+});
+
+Route::post('admins', function(Request $request) {
+    return Admin::create($request->all);
+});
+
+Route::put('admins/{id}', function(Request $request, $id) {
+    $admin = Admin::findOrFail($id);
+    $admin->update($request->all());
+
+    return $admin;
+});
+
+Route::delete('admins/{id}', function($id) {
+    Admin::find($id)->delete();
+
+    return 204;
+});
+
+/* Tours */
+Route::get('tours', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Tour::all();
+});
+ 
+Route::get('tours/{id}', function($id) {
+    return Tour::find($id);
+});
+
+Route::post('tours', function(Request $request) {
+    return Tour::create($request->all);
+});
+
+Route::put('tours/{id}', function(Request $request, $id) {
+    $tour = Tour::findOrFail($id);
+    $tour->update($request->all());
+
+    return $tour;
+});
+
+Route::delete('tours/{id}', function($id) {
+    Tour::find($id)->delete();
 
     return 204;
 });
