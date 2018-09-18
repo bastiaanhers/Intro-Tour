c7be7fdfdf933ad407ae6c6d4a8c705c6aec0c0f
Simon Damminga <99039973@mydavinci.nl>
2018-09-18 14:59:49 +0200

    hot fix

--- intro-tour-client/src/app/components/location-page/location-page.component.ts
+++ intro-tour-client/src/app/components/location-page/location-page.component.ts
@@ -92,9 +92,9 @@ export class LocationPageComponent implements OnInit {
   }
 
   public showWindow(id){
-    document.getElementById(`popup-${id}`).classList.add('active');
+    document.getElementById(`popup-${id}`).style.display = 'block';
   }
   public hideWindow(id){
-    document.getElementById(`popup-${id}`).classList.remove('active');
+    document.getElementById(`popup-${id}`).style.display = 'none';
   }
 }
