17f64d873bce78be21fe770bc67d874945fc027e
Simon Damminga <99039973@mydavinci.nl>
2018-09-25 15:06:08 +0200

    updated seerders, get events

--- intro_tour/database/migrations/2018_06_20_075838_create_events_table.php
+++ intro_tour/database/migrations/2018_06_20_075838_create_events_table.php
@@ -15,8 +15,8 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('trigger');
-            $table->string('action');
+            $table->jsonb('trigger');
+            $table->jsonb('action');
             $table->timestamps();
         });
     }
