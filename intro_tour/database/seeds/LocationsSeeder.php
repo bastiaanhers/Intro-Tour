5b9f34420f3e2bf2c5103e5aa159da1d8eace217
Simon Damminga <99039973@mydavinci.nl>
2018-10-02 14:17:31 +0200

    fixed seeders

--- intro_tour/database/seeds/LocationsSeeder.php
+++ intro_tour/database/seeds/LocationsSeeder.php
@@ -16,10 +16,7 @@ class LocationsSeeder extends Seeder
         [
         'name' => "test position",
         'description' => "this is a test postition",
-        'radius' => [
-            "type" => 'circle',
-            "data" => 12
-        ],
+        'radius' => '{"type": "circle", "data": 12}',
         'longitude' => 51.798856,
         'latitude' => 4.683647,
         ]    
