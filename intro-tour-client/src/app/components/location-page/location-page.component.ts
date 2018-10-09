01f370539fd893adb1333748f95b8ae2024cb434
Simon Damminga <99039973@mydavinci.nl>
2018-10-09 08:33:09 +0200

    popup bij locatie met punten zichtbaar

--- intro-tour-client/src/app/components/location-page/location-page.component.ts
+++ intro-tour-client/src/app/components/location-page/location-page.component.ts
@@ -13,8 +13,9 @@ export class LocationPageComponent implements OnInit {
   private given_answer;
   public question;
   public locations = [];
+  public totalPoints: number = 0;
 
-  ky;kx;dy;dx;km;
+  ky; kx; dy; dx; km;
 
   public events;
 
@@ -32,7 +33,7 @@ export class LocationPageComponent implements OnInit {
 
   isTracking = false;
 
-  constructor(private _eventService: EventService, private _questionService: QuestionService, private _locationService: LocationService) { 
+  constructor(private _eventService: EventService, private _questionService: QuestionService, private _locationService: LocationService) {
     this.getEvents();
   }
 
@@ -42,53 +43,59 @@ export class LocationPageComponent implements OnInit {
     this.z = 18;
     this.icons = [
       {
-        url: '/assets/images/marker.png', 
-        scaledSize: {height: 40,width: 40}
+        url: '/assets/images/marker.png',
+        scaledSize: { height: 40, width: 40 }
       },
       {
-        url: '/assets/images/marker-q.png', 
-        scaledSize: {height: 40,width: 40}
+        url: '/assets/images/marker-q.png',
+        scaledSize: { height: 40, width: 40 }
+      },
+      {
+        url: '/assets/images/marker-q-done.png',
+        scaledSize: { height: 40, width: 40 }
       }
     ];
   }
 
 
-  private trackMe(){  
-    if(navigator.geolocation){
+  private trackMe() {
+    if (navigator.geolocation) {
       this.isTracking = true;
       navigator.geolocation.watchPosition(
-        (position) => {this.showTrackingPosition(position);}, 
-        (err) => {console.error('ERROR(' + err.code + '): ' + err.message);},
-        {maximumAge:600000, timeout:5000, enableHighAccuracy: true}
-      );  
-    }else{
+        (position) => { this.showTrackingPosition(position); },
+        (err) => { console.error('ERROR(' + err.code + '): ' + err.message); },
+        { maximumAge: 600000, timeout: 5000, enableHighAccuracy: true }
+      );
+    } else {
       alert("Je locatie kan helaas niet worden gevonden");
     }
   }
 
-  public showTrackingPosition(position){
+  public showTrackingPosition(position) {
     this.curLocation.x = position.coords.latitude;
     this.curLocation.y = position.coords.longitude;
 
-    this.showHidePopup()
+    //this.showHidePopup()
   }
 
-  public showHidePopup(){
+  public showHidePopup() {
     this.locations.forEach(location => {
-      if(this.arePointsNear(location)){
-        this.showWindow(location);
-      }else if(!this.arePointsNear(location)){
+      if (this.arePointsNear(location)) {
+        if (location.map_icon == 1) {
+          this.showWindow(location);
+        }
+      } else if (!this.arePointsNear(location)) {
         this.hideWindow(location.id);
       }
     });
   }
 
-  public arePointsNear(location){
+  public arePointsNear(location) {
     this.km = location.radius.data / 1000;
 
     this.ky = 40000 / 360;
     this.kx = Math.cos(Math.PI * location.latitude / 180.0) * this.ky;
-    
+
     this.dx = Math.abs(location.longitude - this.curLocation.y) * this.kx;
     this.dy = Math.abs(location.latitude - this.curLocation.x) * this.ky;
 
@@ -96,55 +103,77 @@ export class LocationPageComponent implements OnInit {
   }
 
 
-  public showWindow(location){
+  public showWindow(location) {
     this._questionService.getQuestion(location.question_id)
-        .subscribe((question) => {
-          this.question = question[0];
-          document.getElementById(`popup-${location.id}`).style.display = 'block';
-        });
+      .subscribe((question) => {
+        this.question = question[0];
+        document.getElementById(`popup-${location.id}`).style.display = 'block';
+      });
   }
-  public hideWindow(id){
+  public hideWindow(id) {
     document.getElementById(`popup-${id}`).style.display = 'none';
   }
-  public getEvents(){
+  public getEvents() {
     this._eventService.getEvents()
-        .subscribe((res: any) => {
-          this.getLocation(res);
-        });
+      .subscribe((res: any) => {
+        this.getLocation(res);
+      });
   }
-  public getQuestion(id){
-    this._questionService.getQuestion(id).subscribe((question) => {this.question = question[0]; console.log(this.question)});
+  public getQuestion(id) {
+    this._questionService.getQuestion(id).subscribe((question) => { this.question = question[0]; console.log(this.question) });
   }
-  public getLocation(events){
+  public getLocation(events) {
     events.forEach(event => {
       this._locationService.getLocation(event.event.trigger.data.location_id)
-          .subscribe((res: any) => {
-            res[0].question_id = event.event.action.data.question_id;
-            this.locations.push(res[0]);
-          });
+        .subscribe((res: any) => {
+          res[0].question_id = event.event.action.data.question_id;
+          res[0].points = event.event.action.data.points;
+          res[0].devider = event.event.action.data.devider;
+          res[0].map_icon = 1;
+          this.locations.push(res[0]);
+        });
     });
   }
 
-  public checkAnswer(id){
-    if(this.given_answer == undefined){
+  public checkAnswer(id) {
+    if (this.given_answer == undefined) {
       alert('Je moet wel een antwoord kiezen');
-    }else if(this.given_answer == 1){
-      alert('Goed Gedaan!');
+    } else if (this.given_answer == 1) {
       this.hideWindow(id);
-      this.deleteLocation(id);
-    }else{
-      alert('Helaas');
+      document.getElementById(`right-${id}`).style.display = 'block';
+    } else {
       this.hideWindow(id);
-      this.deleteLocation(id);
+      document.getElementById(`wrong-${id}`).style.display = 'block';
     }
   }
-  public setValue(answer){
+  public setValue(answer) {
     this.given_answer = answer;
   }
-  private deleteLocation(id){ 
+  public tryAgain(location) {
+    this.locations.forEach((array_location, index) => {
+      if(array_location.id == location.id){
+        this.locations[index].points = this.locations[index].points / this.locations[index].devider;
+      }
+    });
+    document.getElementById(`wrong-${location.id}`).style.display = 'none';
+    this.showWindow(location)
+  }
+  public close(id) {
+    document.getElementById(`wrong-${id}`).style.display = 'none';
+    document.getElementById(`right-${id}`).style.display = 'none';
+    if(this.given_answer == 0){
+      this.locations.forEach((location, index) => {
+        if(location.id == id){
+          this.locations[index].points = 0;
+        }
+      });
+    }
+    this.changeMarker(id);
+  }
+  private changeMarker(id) {
     this.locations.forEach((location, index) => {
-      if(location.id == id){
-        this.locations.splice(index, 1);
+      if (location.id == id) {
+        this.locations[index].map_icon = 2;
       }
     });
   }
