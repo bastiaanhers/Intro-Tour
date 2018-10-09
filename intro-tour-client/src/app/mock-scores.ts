9639313c3631d59b14025524c72441dce338c3d4
Jis van Overschot <99040091@mydavinci.nl>
2018-10-09 21:15:13 +0200

    Added leaderboard component with custom pipe to change number formating

---
+++ intro-tour-client/src/app/mock-scores.ts
@@ -0,0 +1,16 @@
+import { Scores } from './scores';
+
+export const SCORES: Scores[] = [
+	{
+		team_name: 'Team Rush A',
+		team_score: 2000
+	},
+	{
+		team_name: 'Team Rush B',
+		team_score: 1000000
+	},
+	{
+		team_name: 'Team Rush C',
+		team_score: 1
+	},
+];
