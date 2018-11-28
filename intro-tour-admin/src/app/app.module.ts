7ca6d8a61f4b8f4508434acd2a5956db229f164b
Simon Damminga <99039973@mydavinci.nl>
2018-11-28 11:23:06 +0100

    added routing

--- intro-tour-admin/src/app/app.module.ts
+++ intro-tour-admin/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { EventComponent } from './components/event/event.component';
+import { NavigateComponent } from './components/navigate/navigate.component';
+
+
+const appRoutes: Routes = [
+  {path: 'event', component: EventComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent,
+    NavigateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
